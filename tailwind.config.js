2418b3616692f2e71d096123f2b100550111c788
yaelahmal <widad.akmal83@gmail.com>
2024-12-01 18:25:43 +0700

    feat: navbar && hero section

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#0E1947",
+        secondary: {
+          300: "#FF5F31",
+          500: "#EE4312",
+        },
+        "blue-gray": "#647EA0",
+        silver: "#E2DEDC",
+        black: "#100F1B",
+        gray: "#7D7D7D",
+      },
+    },
+  },
+  plugins: [],
+};
